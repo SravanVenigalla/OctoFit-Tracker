520c679d3539d172b833dbd9b5cd5743816ac52c
Sravan Kumar Venigalla <112797358+SravanVenigalla@users.noreply.github.com>
2025-04-09 07:07:37 +0000

    Server Runs and api tested

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -6,7 +6,7 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = 'http://localhost:8000/'
+    base_url = 'https://sturdy-funicular-6q7rpj9gj74cx5v7-8000.app.github.dev/'
     return Response({
         'users': base_url + 'api/users/',
         'teams': base_url + 'api/teams/',
